7a169efec82f05e1953fb84a7fcc9873a58c4c53
Mirzaali Abzalov <mirzaali.abzalov@fido.uz>
2025-08-12 18:00:34 +0500

    add some tests

--- selenium/selenium_web.py
+++ selenium/selenium_web.py
@@ -2,6 +2,7 @@ import os
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 # не смог разобраться с прокси, временное решение
 for var in ["HTTP_PROXY", "HTTPS_PROXY", "http_proxy", "https_proxy"]:
     os.environ.pop(var, None)
@@ -11,12 +12,14 @@ options.add_argument("start-maximized")
 
 
 driver = webdriver.Chrome(options=options)
-sleep(3)
+sleep(2)
 driver.maximize_window()
 # driver.set_window_size(1920, 1080)
+# options.add_experimental_option("detach", True)
 driver.get("https://google.com")
-
-# 45-48
+print(driver.title)
+print(driver.current_url)
+# 1-19-27
 
 
 
