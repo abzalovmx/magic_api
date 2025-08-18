e3a4e993351c90a3557076b9c55674f84b686f3f
Mirzaali Abzalov <mirzaali.abzalov@fido.uz>
2025-08-18 15:53:44 +0500

    add some test

--- selenium/selenium_part3.py
+++ selenium/selenium_part3.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pytest
 
 # не смог разобраться с прокси, временное решение
@@ -15,10 +17,11 @@ for var in ["HTTP_PROXY", "HTTPS_PROXY", "http_proxy", "https_proxy"]:
 @pytest.fixture
 def driver():
     chrom_driver = webdriver.Chrome()
-    chrom_driver.implicitly_wait(60)
+    chrom_driver.implicitly_wait(10)
     # sleep(4)
     chrom_driver.maximize_window()
     yield chrom_driver
+    chrom_driver.quit()
 
 
 
@@ -52,15 +55,43 @@ def test_welcome(driver):
 
 
 def test_cart(driver):
-    driver.get('https://uzum.uz/uz/product/erkaklar-uchun-ikki-1596531')
-    size = driver.find_element(By.CLASS_NAME, 'active radio-image-wrapper').click()
-    color = driver.find_element(By.TAG_NAME, '[alt="Koʻk"]').click()
-    button = driver.find_element(By.TAG_NAME, '[data-test-id=button__add-cart]').click()
-    counter = driver.find_element(By.CSS_SELECTOR, 'span.CaptionLRegular')
-    # span.CaptionLRegular
+    driver.get('https://magento.softwaretestingboard.com/argus-all-weather-tank.html')
+    driver.find_element(By.ID, 'option-label-size-143-item-166').click()
+    driver.find_element(By.ID, 'option-label-color-93-item-52').click()
+    button = driver.find_element(By.ID, 'product-addtocart-button')
+    driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", button)
+    button.click()
+    wait = WebDriverWait(driver, 5)
+    wait.until_not(EC.text_to_be_present_in_element_attribute(
+        (By.CSS_SELECTOR, '.counter.qty'),
+        'class',
+        'empty'
+    ))
+    wait.until_not(EC.text_to_be_present_in_element_attribute(
+        (By.CSS_SELECTOR, '.counter.qty'),
+        'class',
+        'loading'
+    ))
+    counter = driver.find_element(By.CSS_SELECTOR, '.counter-number')
+    print(counter.text)
 
 
+def test_5_sec2(driver):
+    driver.get('https://demoqa.com/dynamic-properties')
+    WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.ID, 'visibleAfter')))
+    button3 = driver.find_element(By.ID, 'visibleAfter')
+    button3.click()
+    print(driver.get_cookies())
+    # driver.add_cookie({'name': 'name'})
 
 
+def test_same_el(driver):
+    driver.get('https://magento.softwaretestingboard.com/men/bottoms-men/pants-men.html')
+    product_link = driver.find_elements(By.CLASS_NAME, 'product-item-link')
+    print(product_link[0].text)
 
 
+def test_same_els(driver):
+    driver.get('https://magento.softwaretestingboard.com/men/bottoms-men/pants-men.html')
+    product_link = driver.find_elements(By.CLASS_NAME, 'product-item-info')
+    print(product_link[0].find_element(By.CLASS_NAME, 'price'))
